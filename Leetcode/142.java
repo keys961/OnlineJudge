488621a96242e376693d328ecad1cbd1d3ecc2c8
keys961 <keys961@hotmail.com>
2017-08-29 18:58:21 +0800

    .

---
+++ Leetcode/142.java
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public ListNode detectCycle(ListNode head)
+    {
+        Set<ListNode> set = new HashSet<>();
+        while(head != null)
+        {
+            if(set.contains(head))
+                return head;
+            set.add(head);
+            head = head.next;
+        }
+        return null;
+    }
+}
