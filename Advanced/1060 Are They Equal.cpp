fe0de7326f641ad27193243cf93012d08a9c60ce
keys961 <keys961@hotmail.com>
2017-09-10 16:27:56 +0800

    .

--- Advanced/1060 Are They Equal.cpp
+++ Advanced/1060 Are They Equal.cpp
@@ -1,89 +1,102 @@
-#include <cstdio>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-int getE(char * p)
-{ 
- 	int e = 0;
- 
- 	if (p[0] != '0')
-  		for (int i = 0; p[i] != 0 && p[i] != '.'; i++)
-   			e++;
- 	else if(p[1] == '.')
-  		for (int i = 2; p[i + 1] != 0 && p[i] == '0'; i++)
-   			e--;
- 	else
- 	{
-	  	int j = 0;
-	  	while (p[j] == '0') j++;
-	  	for (; p[j] != 0 && p[j] != '.'; j++)
-	    	e++;
-	}
- 
- 	return e;
-}
-
-string getB(char *p, int n,int e)
-{ 
- 	string ret = "";
- 	int idx = 0;
- 	if (e > 0)
- 	{
-  		int i = 0;
-  		while (p[i] == '0')i++;
-  		for (; p[i] != 0 && idx <n; i++)
-  		{
-	   		if (p[i] == '.')continue;
-	   		ret += p[i];
-	   		idx++;
-	 	}
-	}
-	else 
-	{
-	  	int i = 0;
-	  	if (p[i + 1] == 0)
-	  	{
-	   		ret = "0";
-	   		while (ret.size() < n)
-	    		ret += '0';
-	   	}
-	   	return ret;
-	 }
-	 while (p[i] != '.')i++;
-	 ++i;
-	 while (p[i] == '0')i++;  //0.000x的问题
-	 for (; p[i] != 0 && idx < n; i++)
-	 {
-	   	ret += p[i];
-	   	idx++;
-	 }
-	 
-	 while (ret.size() < n)
-	  	ret += '0';
-	 return ret;
-}
+int getExponent(string num);
+string getBase(string num, int n, int e);
 
 int main()
 {
-	 int N;
-	 char A[101];
-	 char B[101];
-	 scanf("%d %s %s", &N, A, B);
-	 string A2;
-	 string B2;
-	 int Ae = getE(A);
-	 int Be = getE(B);
-	 A2 = getB(A, N,Ae);
-	 B2 = getB(B, N,Be);
-	 if (N == 0)
-	 	 printf("YES 0.*10^0\n");
-	 
-	 else if (Ae == Be && A2 == B2)
-	  	printf("YES 0.%s*10^%d\n", A2.c_str(), Ae);
-	 
-	 else
-	  	printf("NO 0.%s*10^%d 0.%s*10^%d\n", A2.c_str(), Ae, B2.c_str(), Be);
-	 
-	 return 0;
+	int n;
+	string num1, num2;
+	cin >> n >> num1 >> num2;
+	int exp1 = getExponent(num1);
+	int exp2 = getExponent(num2);
+	string base1 = getBase(num1, n, exp1);
+	string base2 = getBase(num2, n, exp2);
+	if (n == 0)
+		printf("YES 0.*10^0\n");
+	else if (exp1 == exp2 && base1 == base2)
+		printf("YES 0.%s*10^%d\n", base1.c_str(), exp1);
+	else
+		printf("NO 0.%s*10^%d 0.%s*10^%d\n", base1.c_str(), exp1, base2.c_str(), exp2);
+	
+	return 0;
+}
+
+int getExponent(string num)
+{
+	int exponent = 0;
+	while (num[0] == '0')
+		num.erase(0, 1);
+	if (num.size() == 0)
+		return 0;
+	if (num[0] != '.')
+	{
+		for (int i = 0; num[i] && num[i] != '.'; i++)
+			exponent++;
+		return exponent;
+	}
+	else
+	{
+		bool flag = false;
+		for (int i = 1; num[i]; i++)
+		{
+			if(num[i] == '0')
+				exponent--;
+			else
+			{
+				flag = true;
+				break;
+			}
+		}
+		if(flag)
+			return exponent;
+		return  0;
+	}
+}
+
+string getBase(string num, int n, int e)
+{
+	string res = "";
+	int index = 0;
+	if (e > 0)
+	{
+		int i = 0;
+		while (num[i] == '0')
+			i++;
+		for (; num[i] && index < n; i++)
+		{
+			if (num[i] == '.')
+				continue;
+			res += num[i];
+			index++;
+		}
+	}
+	else
+	{
+		int i = 0;
+		if (num[i + 1] == 0)
+		{
+			res = "0";
+			while (res.size() < n)
+				res += '0';
+			return res;
+		}
+		while (num[i] != '.')
+			i++;
+		i++;
+		while (num[i] == '0')
+			i++;
+		for (; num[i] && index < n; i++)
+		{
+			res += num[i];
+			index++;
+		}
+	}
+
+	while (res.size() < n)
+		res += '0';
+	return res;
 }
