df7c065198b6239214fa63713fc90c9dc1143e85
keys961 <keys961@hotmail.com>
2017-09-15 16:30:17 +0800

    .

--- Advanced/1129 Recommendation System.cpp
+++ Advanced/1129 Recommendation System.cpp
@@ -1,66 +1,64 @@
 #include <cstdio>
-#include <queue>
-#include <vector>
-#include <algorithm>
 #include <set>
 
 using namespace std;
 
 struct Item
 {
-    int id;
-    int freq;
+	int id;
+	int freq;
 
-    Item(int id, int freq)
-    {
-        this->id = id;
-        this->freq = freq;
-    }
+	Item(int id, int freq)
+	{
+		this->id = id;
+		this->freq = freq;
+	}
 
-    friend bool operator<(const Item& a, const Item& b)
-    {
-        if(a.freq == b.freq)
-            return a.id < b.id;
-        return a.freq > b.freq;
-    }
+	friend bool operator< (const Item& a, const Item& b)
+	{
+		if (a.freq == b.freq)
+			return a.id < b.id;
+		return a.freq > b.freq;
+	}
 };
 
 int frequency[50001];
+set<Item> items;
 
-int main(int argc, char const *argv[])
+int main()
 {
-    int n, k, id;
-    scanf("%d %d", &n, &k);
-    set<Item> itemList;
-    for(int i = 0; i < n; i++)
-    {
-        scanf("%d", &id);
-        frequency[id]++;
-        set<Item>::iterator it;
-        if(i > 0)
-        {
-            vector<int> res;
-            for (it = itemList.begin(); it != itemList.end(); it++)
-            {
-                if (it->freq == 0)
-                    break;
-                res.push_back(it->id);
-                if (res.size() == k)
-                    break;
-            }
-            if (res.empty())
-                continue;
-            printf("%d:", id);
-            for (int i = 0; i < res.size(); i++)
-                printf(" %d", res[i]);
-            printf("\n");
-        }
+	int n, k;
+	scanf("%d %d", &n, &k);
+	set<Item>::iterator it;
 
-        Item item(id, frequency[id] - 1);
-        if((it = itemList.find(item)) != itemList.end())
-            itemList.erase(it);
-        item.freq += 1;
-        itemList.insert(item);
-    }
-    return 0;
+	for (int i = 0; i < n; i++)
+	{
+		int val;
+		scanf("%d", &val);
+
+		if (i != 0)
+		{
+			printf("%d:", val);
+			it = items.begin();
+			for (int j = 0; j < k && it != items.end(); j++)
+			{
+				if (it->freq == 0)
+					break;
+				printf(" %d", it->id);
+				it++;
+			}
+			printf("\n");
+		}
+		
+		Item item(val, frequency[val]);
+		
+		if ((it = items.find(item)) != items.end())
+			items.erase(it);
+		frequency[val]++;
+		item.freq++;
+		items.insert(item);
+
+	}
+
+	return 0;
 }
--- Advanced/1131 Subway Map.cpp
+++ Advanced/1131 Subway Map.cpp
@@ -1,135 +1,108 @@
-// Like this question, if dijkstra is complicated, use dfs
-
-#include <iostream>
-#include <vector>
+#include <cstdio>
 #include <cstring>
-
-#define MAX 0x7fffffff
+#include <vector>
+#include <map>
 
 using namespace std;
 
 struct Station
 {
-    int id;
-    int lineId;
-    Station(int id, int lineId)
-    {
-        this->id = id;
-        this->lineId = lineId;
-    }
+	int id;
+	int lineId; //Key point
+	Station(int id, int lineId): id(id), lineId(lineId){}
 };
 
-struct Node
+struct Segment 
 {
-    vector<Station> adjStations;
+	int lineId;
+	int start, end;
+	Segment(int lineId, int start, int end) :
+		lineId(lineId), start(start), end(end){}
 };
 
-struct Segment
-{
-    int lineId;
-    int start, end;
-    Segment(int lineId, int start, int end)
-    {
-        this->lineId = lineId;
-        this->start = start;
-        this->end = end;
-    }
-};
-
-struct Answer
-{
-    vector<Segment> list;
-    int length;
-    bool operator < (const Answer& a) const
-    {
-        if(this->length == a.length)
-            return list.size() < a.list.size();
-        return this->length < a.length;
-    }
-};
-
-Node subwayMap[10001]; //Graph with lineId, adj list
+map< int, vector<Station> > subwayMap;
 bool visited[10001];
 
-Answer tempAns, finalAns;
+vector<Segment> ans;
+int ansLen = 0x7fffffff;
+
+void dfs(int src, int dst, vector<Segment> tempAns, int tempLen);//Key point
 
 void init()
 {
-    memset(visited, false, sizeof(visited));
-    tempAns.list.clear();
-    tempAns.length = 0;
-    finalAns.list.clear();
-    finalAns.length = MAX;
+	memset(visited, false, sizeof(visited));
+	ans.clear();
+	ansLen = 0x7fffffff;
 }
 
-void dfs(int src, int dst);
-
-int main(int argc, char** argv)
+int main()
 {
+	int n, m, k;
+	scanf("%d", &n);
+	for (int line = 1; line <= n; line++)
+	{
+		scanf("%d", &m);
+		int v1, v2;
+		scanf("%d", &v1);
+		for (int i = 1; i < m; i++)
+		{
+			scanf("%d", &v2);
+			subwayMap[v1].push_back(Station(v2, line));
+			subwayMap[v2].push_back(Station(v1, line));
+			v1 = v2;
+		}
+	}
 
-    int n, m, k;
-    cin >> n;
-    for(int i = 1; i <= n; i++)
-    {
-        cin >> m;
-        int pre, cur;
-        cin >> pre;
-        for(int j = 1; j < m; j++)
-        {
-            cin >> cur;
-            subwayMap[pre].adjStations.push_back(Station(cur, i));
-            subwayMap[cur].adjStations.push_back(Station(pre, i));
-            pre = cur;
-        }
-    }
+	scanf("%d", &k);
+	for (int i = 0; i < k; i++)
+	{
+		int src, dst;
+		scanf("%d %d", &src, &dst);
+		init();
+		visited[src] = true;
+		dfs(src, dst, vector<Segment>(), 0);
+		printf("%d\n", ansLen);
+		for (int i = 0; i < ans.size(); i++)
+			printf("Take Line#%d from %04d to %04d.\n", ans[i].lineId,
+				ans[i].start, ans[i].end);
+	}
 
-    cin >> k;
-    for(int i = 0; i < k; i++)
-    {
-        init();
-        int src, dst;
-        cin >> src >> dst;
-        dfs(src, dst);
-        cout << finalAns.length << endl;
-
-        for(int i = 0; i < finalAns.list.size(); i++)
-            printf("Take Line#%d from %04d to %04d.\n",
-            finalAns.list[i].lineId, finalAns.list[i].start,
-            finalAns.list[i].end);
-    }
-    return 0;
+	return 0;
 }
 
-void dfs(int src, int dst)
+void dfs(int src, int dst, vector<Segment> tempAns, int tempLen)
 {
-    if(src == dst)
-    {
-        if(tempAns < finalAns)
-            finalAns = tempAns;
-    }
+	if (src == dst)
+	{
+		if (tempLen < ansLen)
+		{
+			ans = tempAns;
+			ansLen = tempLen;
+		}
+		else if (tempLen == ansLen)
+		{
+			if (tempAns.size() < ans.size())
+				ans = tempAns;
+		}
+	}
 
-    if(finalAns < tempAns)
-        return;
+	if (tempLen > ansLen || (tempLen == ansLen && tempAns.size() > ans.size()))
+		return;
 
-    for(int i = 0; i < subwayMap[src].adjStations.size(); i++)
-    {
-        int next = subwayMap[src].adjStations[i].id;
-        if(!visited[next])
-        {
-            Answer temp = tempAns;
-            tempAns.length++;
-            if(tempAns.list.size() == 0 || subwayMap[src].adjStations[i].lineId !=
-                    tempAns.list.back().lineId)
-                tempAns.list.push_back(Segment(subwayMap[src].adjStations[i].lineId,
-                           src, next));
-            else
-                tempAns.list.back().end = next;
-
-            visited[next] = true;
-            dfs(next, dst);
-            //Restore
-            visited[next] = false;
-            tempAns = temp;
-        }
-    }
+	for (int i = 0; i < subwayMap[src].size(); i++)
+	{
+		int next = subwayMap[src][i].id;
+		if (!visited[next])
+		{
+			vector<Segment> t = tempAns;
+			if (tempAns.size() == 0 || subwayMap[src][i].lineId != tempAns.back().lineId)//Change
+				tempAns.push_back(Segment(subwayMap[src][i].lineId, src, next));
+			else //Not change
+				tempAns[tempAns.size() - 1].end = next;
+			visited[next] = true;
+			dfs(next, dst, tempAns, tempLen + 1);
+			visited[next] = false;
+			tempAns = t;
+		}
+	}
 }
